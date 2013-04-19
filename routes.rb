0c35cce3faac0c01c2aeaa95b13e3b1eaf84cbd7
Jake Parks <jakob.parks@gmail.com>
2013-04-19 16:04:53 -0700

    master: set up zero route

--- routes.rb
+++ routes.rb
@@ -3,5 +3,5 @@ GitTest::Application.routes.draw do
 
   root :to => 'simulations#simulate'
   match '/simulate', to: 'simulations#simulate', via: [:get, :post]
-  get 'simulation_result_test', to: 'simulations#simulation_result_test'
+  get 'zero', to: 'simulations#zero'
 end
