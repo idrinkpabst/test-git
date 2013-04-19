0b7b20c67fb8bc9c87c03e7560f95cffdd4716c1
Jake Parks <jakob.parks@gmail.com>
2013-04-19 16:02:23 -0700

    guava: make route guava

--- routes.rb
+++ routes.rb
@@ -1,7 +1,7 @@
 GitTest::Application.routes.draw do
   devise_for :users
 
-  root :to => 'simulations#simulate'
+  root :to => 'guava'
   match '/simulate', to: 'simulations#simulate', via: [:get, :post]
   get 'simulation_result_test', to: 'simulations#simulation_result_test'
 end
